b73962d940b4201d8e8259d1022ed79b885d2173
Justin Beghtol <Justin.Beghtol@embodied.me>
2025-01-06 16:22:03 -0800

    Fixed deploy comments.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo "Building and pushing 'latest'"
-echo docker-compose build
-echo docker-compose push
+docker-compose build
+docker-compose push
 export OPENMOXIE_VERSION=$(cat site/VERSION)
 echo "Building and pushing '$OPENMOXIE_VERSION'"
-echo docker-compose build
-echo docker-compose push
+docker-compose build
+docker-compose push
